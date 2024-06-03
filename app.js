af36aec8effb6ef51bfc268e6f56f3e0bd9e6a56
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-03 16:41:09 +0500

    js done
    continue

---
+++ app.js
@@ -0,0 +1,45 @@
+let display = document.getElementById("display");
+let on = document.getElementById("on");
+let off = document.getElementById("off");
+let clear = document.getElementById("clear");
+let count = 0;
+let milliseconds = 0;
+let sec = 0;
+let min = 0;
+let hour = 0;
+let show ;
+
+function counter(){
+    count++;
+    milliseconds++;
+    if(milliseconds===100){
+        sec++;
+        milliseconds=0;
+        if(sec===60){
+            min++;
+            sec=0;
+            if(min===60){
+               hour++;
+               min=0;
+        
+            }
+        }
+    }
+    display.innerText = `${hour} ${min} ${sec} ${milliseconds}`;
+}
+
+function start(){
+    show = setInterval(counter, 10)
+}
+
+function pause(){
+    clearInterval(show);
+}
+
+function reset(){
+    clearInterval(show);
+    sec = 0;
+    min = 0;
+    hour = 0;
+    display.innerText = `00 : 00 : 00 :00`;
+}
